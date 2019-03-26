d2a5055f4cd88229f8d1883726e33390d71fa3bf
satya-01 <48516194+satya-01@users.noreply.github.com>
2019-03-26 17:31:41 +0530

    first commit

---
+++ print coade.py
@@ -0,0 +1,3 @@
+Python 3.8.0a1 (tags/v3.8.0a1:e75eeb00b5, Feb  3 2019, 20:47:39) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("hello every one")
